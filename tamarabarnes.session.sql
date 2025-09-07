5b7caf7b2d3bab67a8a20cd1cacd82055af35010
Tamara Barnes <tammy.barnes912@gmail.com>
2025-09-07 17:07:12 +1000

    added DOB column to customers table

--- tamarabarnes.session.sql
+++ tamarabarnes.session.sql
@@ -14,7 +14,8 @@ CREATE TABLE customers (
     customer_id SERIAL PRIMARY KEY,
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
-    email VARCHAR(255) NOT NULL,
+    dob DATE NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
     phone VARCHAR(15) NOT NULL,
     deleted_at TIMESTAMP DEFAULT NULL
 );
