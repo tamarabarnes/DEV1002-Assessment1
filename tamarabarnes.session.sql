0559777aae425a205ad7a026cd11614f17d1d402
Tamara Barnes <tammy.barnes912@gmail.com>
2025-09-09 09:05:14 +1000

    fixed error on bookings table

--- tamarabarnes.session.sql
+++ tamarabarnes.session.sql
@@ -120,9 +120,9 @@ CREATE TABLE bookings (
     customer_id INTEGER NOT NULL,
     service_id INTEGER NOT NULL,
     walker_id INTEGER NOT NULL,
-    FOREIGN KEY customer_id REFERENCES customers(customer_id) ON DELETE RESTRICT,
-    FOREIGN KEY service_id REFERENCES services(service_id) ON DELETE RESTRICT,
-    FOREIGN KEY walker_id REFERENCES dog_walker(walker_id) ON DELETE RESTRICT
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id) ON DELETE RESTRICT,
+    FOREIGN KEY (service_id) REFERENCES services(service_id) ON DELETE RESTRICT,
+    FOREIGN KEY (walker_id) REFERENCES dog_walker(walker_id) ON DELETE RESTRICT
 );
 -- Creating payments table 
 CREATE TABLE payments (
